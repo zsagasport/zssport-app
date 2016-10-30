8389adde0e4ceb4140ed8f1e5e42d54f60cd26b4
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-10-30 16:48:21 +0100

    Add templates based on 'matchType'

--- app/match.component.ts
+++ app/match.component.ts
@@ -5,16 +5,28 @@ import { MatchService } from "./match.service";
 @Component({
     selector: 'zssport-match',
     template:`
-        <div class="match-content" *ngIf="match">
-            <div class="teams">
-                <label class="home-club">{{match.homeClub}}</label>
-                <label class="away-club">{{match.awayClub}}</label>
-            </div>
-            <div class="results finished" *ngIf="match.finished">
-                <span>{{match.homeGoals}}</span>
-                <span>-</span>
-                <span>{{match.awayGoals}}</span>
-            </div>
+        <div class="match-content" *ngIf="match" [ngSwitch]="matchType">
+            <template [ngSwitchCase]="'lined'">
+                <div class="home-team {{match.homeGoals > match.awayGoals ? 'winner' : ''}}" >
+                    <label class="home-club">{{match.homeClub}}</label>
+                    <span class="goals">{{match.homeGoals}}</span>
+                </div>
+                <div class="away-team {{match.awayGoals > match.homeGoals ? 'winner' : ''}}" *ngIf="match.finished">
+                    <label class="away-club">{{match.awayClub}}</label>
+                    <span class="goals">{{match.awayGoals}}</span>
+                </div>
+            </template>
+            <template ngSwitchDefault>
+                <div class="teams">
+                    <label class="home-club">{{match.homeClub}}</label>
+                    <label class="away-club">{{match.awayClub}}</label>
+                </div>
+                <div class="results finished" *ngIf="match.finished">
+                    <span>{{match.homeGoals}}</span>
+                    <span>-</span>
+                    <span>{{match.awayGoals}}</span>
+                </div>
+            </template>
         </div>
 `
 })
