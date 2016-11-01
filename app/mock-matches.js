eab42f7a57859a13a60df4c740f8b9f338c59689
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-11-01 07:51:40 +0100

    Regenerated

--- app/mock-matches.js
+++ app/mock-matches.js
@@ -2,7 +2,7 @@
 var ARSENAL = "Arsenal", BOURNEMOUTH = "Bournemouth", BURNLEY = "Burnley FC", CHELSEA = "Chelsea", CRYSTAL = "Crystal Palace", LEICESTER = "Leicester City", HULL = "Hull City", SWANSEA = "Swansea City", MCITY = "Manchester City", MIDDLESBROUGH = "Middlesbrough", MUNITED = "Man United", EVERTON = "Everton", LIVERPOOL = "Liverpool FC", TOTTENHAM = "Tottenham", SOUTHAMPTON = "Southampton", STOKE = "Stoke City", SUNDERLAND = "Sunderland", WATFORD = "Watford", WESTBROM = "West Brom", WESTHAM = "West Ham";
 exports.MATCHES = [
     {
-        id: 201617010901,
+        id: 201617010801,
         homeClub: CHELSEA,
         homeGoals: 3,
         awayClub: LEICESTER,
@@ -12,7 +12,7 @@ exports.MATCHES = [
         round: 8
     },
     {
-        id: 201617010902,
+        id: 201617010802,
         homeClub: ARSENAL,
         homeGoals: 3,
         awayClub: SWANSEA,
@@ -22,7 +22,7 @@ exports.MATCHES = [
         round: 8
     },
     {
-        id: 201617010903,
+        id: 201617010803,
         homeClub: WESTBROM,
         homeGoals: 1,
         awayClub: TOTTENHAM,
@@ -32,7 +32,7 @@ exports.MATCHES = [
         round: 8
     },
     {
-        id: 201617010904,
+        id: 201617010804,
         homeClub: STOKE,
         homeGoals: 2,
         awayClub: SUNDERLAND,
@@ -42,7 +42,7 @@ exports.MATCHES = [
         round: 8
     },
     {
-        id: 201617010905,
+        id: 201617010805,
         homeClub: BOURNEMOUTH,
         homeGoals: 6,
         awayClub: HULL,
@@ -52,7 +52,7 @@ exports.MATCHES = [
         round: 8
     },
     {
-        id: 201617010906,
+        id: 201617010806,
         homeClub: MCITY,
         homeGoals: 1,
         awayClub: EVERTON,
@@ -62,7 +62,7 @@ exports.MATCHES = [
         round: 8
     },
     {
-        id: 201617010907,
+        id: 201617010807,
         homeClub: CRYSTAL,
         homeGoals: 0,
         awayClub: WESTHAM,
@@ -72,7 +72,7 @@ exports.MATCHES = [
         round: 8
     },
     {
-        id: 201617010908,
+        id: 201617010808,
         homeClub: MIDDLESBROUGH,
         homeGoals: 0,
         awayClub: WATFORD,
@@ -82,7 +82,7 @@ exports.MATCHES = [
         round: 8
     },
     {
-        id: 201617010909,
+        id: 201617010809,
         homeClub: SOUTHAMPTON,
         homeGoals: 3,
         awayClub: BURNLEY,
@@ -92,7 +92,7 @@ exports.MATCHES = [
         round: 8
     },
     {
-        id: 201617010910,
+        id: 201617010810,
         homeClub: LIVERPOOL,
         homeGoals: 0,
         awayClub: MUNITED,
@@ -100,6 +100,105 @@ exports.MATCHES = [
         date: '2016/10/17',
         finished: true,
         round: 8
+    }, {
+        id: 201617010901,
+        homeClub: BOURNEMOUTH,
+        homeGoals: 0,
+        awayClub: TOTTENHAM,
+        awayGoals: 0,
+        finished: true,
+        date: '2016/10/22',
+        round: 9
+    },
+    {
+        id: 201617010902,
+        homeClub: ARSENAL,
+        homeGoals: 0,
+        awayClub: MIDDLESBROUGH,
+        awayGoals: 0,
+        finished: true,
+        date: '2016/10/22',
+        round: 9
+    },
+    {
+        id: 201617010903,
+        homeClub: LEICESTER,
+        homeGoals: 3,
+        awayClub: CRYSTAL,
+        awayGoals: 1,
+        date: '2016/10/22',
+        finished: true,
+        round: 9
+    },
+    {
+        id: 201617010904,
+        homeClub: WESTHAM,
+        homeGoals: 1,
+        awayClub: SUNDERLAND,
+        awayGoals: 0,
+        date: '2016/10/22',
+        finished: true,
+        round: 9
+    },
+    {
+        id: 201617010905,
+        homeClub: HULL,
+        homeGoals: 0,
+        awayClub: STOKE,
+        awayGoals: 2,
+        date: '2016/10/22',
+        finished: true,
+        round: 9
+    },
+    {
+        id: 201617010906,
+        homeClub: SWANSEA,
+        homeGoals: 0,
+        awayClub: WATFORD,
+        awayGoals: 0,
+        date: '2016/10/22',
+        finished: true,
+        round: 9
+    },
+    {
+        id: 201617010907,
+        homeClub: BURNLEY,
+        homeGoals: 2,
+        awayClub: EVERTON,
+        awayGoals: 1,
+        date: '2016/10/22',
+        finished: true,
+        round: 9
+    },
+    {
+        id: 201617010908,
+        homeClub: LIVERPOOL,
+        homeGoals: 2,
+        awayClub: WESTBROM,
+        awayGoals: 1,
+        date: '2016/10/22',
+        finished: true,
+        round: 9
+    },
+    {
+        id: 201617010909,
+        homeClub: MCITY,
+        homeGoals: 1,
+        awayClub: SOUTHAMPTON,
+        awayGoals: 1,
+        date: '2016/10/23',
+        finished: true,
+        round: 9
+    },
+    {
+        id: 201617010910,
+        homeClub: CHELSEA,
+        homeGoals: 4,
+        awayClub: MUNITED,
+        awayGoals: 0,
+        date: '2016/10/23',
+        finished: true,
+        round: 9
     }
 ];
 //# sourceMappingURL=mock-matches.js.map
