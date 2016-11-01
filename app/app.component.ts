2fa9e5f16f9ae7715c31adf1cdc0b9041d1c3a14
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-11-01 07:55:31 +0100

    Show more rounds

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,13 +8,17 @@ import { MatchService } from "./match.service";
         <h1>ZsSport Application</h1>
         <div class="col-md-4">
             <h2>Matches requested by id</h2>
-            <zssport-match [matchId]="201617010901" [matchType]="'lined'"></zssport-match>
-            <zssport-match [matchId]="201617010902"></zssport-match>
-            <zssport-match [matchId]="201617010910"></zssport-match>
+            <zssport-match [matchId]="201617010801" [matchType]="'lined'"></zssport-match>
+            <zssport-match [matchId]="201617010802"></zssport-match>
+            <zssport-match [matchId]="201617010810"></zssport-match>
         </div>
         <div class="col-md-4">
              <h2>Matches requested by round</h2>
              <zssport-round [roundId]="2016170208"></zssport-round>
+        </div>
+         <div class="col-md-4">
+             <h2>Matches requested by round</h2>
+             <zssport-round [roundId]="2016170209"></zssport-round>
         </div>
     `
 })
