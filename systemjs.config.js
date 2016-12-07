85bee74754090dd782676e646500aff5ea11f323
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-12-07 11:52:44 +0100

    Added cors to systemjs config

--- systemjs.config.js
+++ systemjs.config.js
@@ -1,5 +1,5 @@
 /**
- * System configuration for Angular samples
+ * System configuration for Angular2 application
  * Adjust as necessary for your application needs.
  */
 (function (global) {
@@ -22,14 +22,19 @@
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
       // other libraries
-      'rxjs':                      'npm:rxjs',
-      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
+      'cors':                       'npm:cors',
+      'rxjs':                       'npm:rxjs',
+      'angular-in-memory-web-api':  'npm:angular-in-memory-web-api',
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
       app: {
         main: './main.js',
         defaultExtension: 'js'
+      },
+      cors: {
+          main: './lib/index.js',
+          defaultExtension: 'js'
       },
       rxjs: {
         defaultExtension: 'js'
